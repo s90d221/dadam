c3a58314127a6687547a75a114ada4c076191e9e
ParkMinGyu <a01048844358@gmail.com>
2025-11-11 17:19:11 +0900

    directory

---
+++ src/main/java/com/dadam/family/DadamApplication.java
@@ -0,0 +1,13 @@
+package com.example.dadambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadamBackendApplication.class, args);
+    }
+
+}
