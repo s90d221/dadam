dcd15a0d882de41f926c38d4850b650267f04989
rinko <happyuo@naver.com>
2025-11-20 06:24:23 +0900

    feat(auth): 사용자 조회 메서드 추가

--- src/main/java/com/example/dadambackend/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/dadambackend/domain/user/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.dadambackend.domain.user.repository;
 import com.example.dadambackend.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
